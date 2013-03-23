93180fbb0d3e0badaa1567bae3f6ff9dce617a38
foest <foest88@gmail.com>
2013-03-23 14:16:58 -0700

    update main, init_category

--- trivia_game.hs
+++ trivia_game.hs
@@ -7,10 +7,15 @@ import Data.List
 
 main = do
   conn <- connectSqlite3 "clues.db"
+  init_category conn
   disconnect conn
   return ()
 
 init_category connection = do
+  show_db_tables connection
   randomCategory <- (randomRIO(1,215828) :: IO Integer)
   stmt <- prepare connection ("SELECT category, value, clue, answer FROM clues JOIN documents ON clues.id = documents.id JOIN classifications ON clues.id = classifications.clueid JOIN categories ON catid = categories.id WHERE categories.id=" ++ show randomCategory)
+  execute stmt []
+  statementResults <- fetchAllRowsAL' stmt
+  show_statement_results statementResults
   return ()
