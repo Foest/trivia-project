87fac67a73d5fdeb9a40df3591251aa343c4bcaf
foest <foest88@gmail.com>
2013-03-23 15:22:09 -0700

    renamed/commented functions

--- trivia_game.hs
+++ trivia_game.hs
@@ -7,12 +7,12 @@ import Data.List
 
 main = do
   conn <- connectSqlite3 "clues.db"
-  init_category conn
+  get_random_category conn
   disconnect conn
   return ()
 
-init_category connection = do
-  show_db_tables connection
+--grabs a random category from db, prints the questions
+get_random_category connection = do
   randomCategory <- (randomRIO(1,215828) :: IO Integer)
   stmt <- prepare connection ("SELECT category, value, clue, answer FROM clues JOIN documents ON clues.id = documents.id JOIN classifications ON clues.id = classifications.clueid JOIN categories ON catid = categories.id WHERE categories.id=" ++ show randomCategory)
   execute stmt []
